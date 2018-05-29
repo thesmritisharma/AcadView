7785c051fb5f2b8805d72aaccb21b3153dbe6c33
Smriti Sharma <smriti7545@gmail.com>
2018-05-30 03:57:07 +0530

    Question 1

---
+++ print1.py
@@ -0,0 +1,16 @@
+print("My first python command")
+print("Acad" + "View")
+x = input("Enter value of x:")
+y = input("Enter value of y:")
+z = input("Enter value of z:")
+print("x=", x)
+print("y=", y)
+print("z=", z)
+print("Let's get started")
+s = "Acadview"
+course = "Python"
+fees = 5000
+print(s, course, fees)
+name = "Tony Stark"
+salary = 1000000
+print("%s %d" % (name, salary))
