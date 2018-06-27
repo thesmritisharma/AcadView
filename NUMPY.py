0740f2c48c4075a8dbb72d20aa4404f0e2941463
Smriti Sharma <smriti7545@gmail.com>
2018-06-27 17:46:40 +0530

    NUMPY 22 June

---
+++ NUMPY.py
@@ -0,0 +1,20 @@
+import numpy as np
+'''
+#Question 1
+a = np.random.rand(10, 1)
+print(np.divide(np.sum(a), 10))
+
+
+#Question 2
+a = np.random.rand(20, 1)
+print('Variation:', np.var(a))
+print('Standard Deviation:', np.std(a))
+
+
+#Question 3
+a = np.random.rand(10, 20)
+b = np.random.rand(20, 25)
+c = np.matmul(a, b)
+print(c)
+print(np.sum(c))
+'''
